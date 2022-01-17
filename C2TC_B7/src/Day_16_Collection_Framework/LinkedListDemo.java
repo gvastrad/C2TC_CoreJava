655370190baef7048c6acd005724368834596b10
Gayatri Vastrad <Gayatri Vastrad@LAPTOP-H2HHKS0N>
2022-01-17 11:49:47 +0530

    first commit

---
+++ C2TC_B7/src/Day_16_Collection_Framework/LinkedListDemo.java
@@ -0,0 +1,25 @@
+package Day_16_Collection_Framework;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList ll=new LinkedList();
+		ll.add(new Integer(10));
+		ll.add("Hi");
+		ll.addFirst("Hello");
+		ll.add(0, 20);
+		ll.add(2,30);
+		//System.out.println(ll);
+		Iterator i1=ll.iterator();
+		while(i1.hasNext())
+		{
+			Object o=i1.next();
+			System.out.println(o);
+			
+		}
+
+	}
+
+}
